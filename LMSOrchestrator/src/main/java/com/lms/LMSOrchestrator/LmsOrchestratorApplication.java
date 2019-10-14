7ef70d9536e8c5abd3112d458576f3fa98d0bede
jobysanthosh <joby.santhosh@smoothstack.com>
2019-10-14 11:39:30 -0400

    Orchestrator set up

---
+++ LMSOrchestrator/src/main/java/com/lms/LMSOrchestrator/LmsOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.lms.LMSOrchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsOrchestratorApplication.class, args);
+	}
+
+}
