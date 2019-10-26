0fd86a2823f01c2e56ac053e5d750a8594798a2e
jobysanthosh <joby.santhosh@smoothstack.com>
2019-10-26 13:42:40 -0400

    properties changed

--- LMSOrchestrator/src/main/java/com/lms/LMSOrchestrator/LmsOrchestratorApplication.java
+++ LMSOrchestrator/src/main/java/com/lms/LMSOrchestrator/LmsOrchestratorApplication.java
@@ -11,7 +11,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,7 +22,6 @@ import org.springframework.web.client.RestTemplate;
 public class LmsOrchestratorApplication {
 
 	@Bean
-	@LoadBalanced
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
